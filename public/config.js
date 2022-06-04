fe63828cd4308219968f48fa0c324e11a5266729
changqi234 <changqi0234@163.com>
2022-06-04 15:13:29 +0800

    修改配置

--- public/config.js
+++ public/config.js
@@ -2,12 +2,12 @@ var BASE = {
     /**
      * @description api请求基础路径
      */
-    //  http://121.196.44.29:8001/
+    //  http://121.196.44.29:8999/
     // http://api.csscmeta.com
     API_DEV: {
-      manager: "http://121.196.44.29:8001/" // 配置请求路径 DEV
+      manager: "http://api.csscmeta.com" // 配置请求路径 DEV
     },
     API_PROD: {
-       manager: "http://121.196.44.29:8001/"  // PROD
+       manager: "http://api.csscmeta.com"  // PROD
     }
   };
