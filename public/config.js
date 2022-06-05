e3090124debf88818150634af74d8fc1674e22e9
changqi234 <changqi0234@163.com>
2022-06-06 01:50:54 +0800

    commit

--- public/config.js
+++ public/config.js
@@ -2,8 +2,9 @@ var BASE = {
     /**
      * @description api请求基础路径
      */
-    //  http://121.196.44.29:8999/
-    // http://api.csscmeta.com
+    //  http://121.196.44.29:8001/ 预生产
+    //  http://121.196.44.29:8999/ 开发
+    // http://api.csscmeta.com 生产
     API_DEV: {
       manager: "http://api.csscmeta.com" // 配置请求路径 DEV
     },
