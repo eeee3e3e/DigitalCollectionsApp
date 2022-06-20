260f5a91ee284cbac1d57927b52a9513476d3ce3
changqi234 <changqi0234@163.com>
2022-06-20 21:22:27 +0800

    修改生产配置

--- public/config.js
+++ public/config.js
@@ -8,13 +8,13 @@ var BASE = {
     // http://resource.csscmeta.com // 生产图片
     API_DEV: {
       // manager: "http://121.196.44.29:8999",
-      manager: "http://api.csscmeta.com", // 配置请求路径 DEV
+      manager: "https://apitest.csscmeta.com", // 配置请求路径 DEV
       // managerImage: "https://citydemo.oss-cn-shenzhen.aliyuncs.com/"
       managerImage: "https://oss.csscmeta.com/"
     },
     API_PROD: {
-       // manager: "http://121.196.44.29:8999",
-       manager: "http://api.csscmeta.com",  // PROD
+      //  manager: "http://121.196.44.29:8999",
+       manager: "https://apitest.csscmeta.com",  // PROD
       //  managerImage: "https://citydemo.oss-cn-shenzhen.aliyuncs.com/"
        managerImage: "https://oss.csscmeta.com/"
     }
